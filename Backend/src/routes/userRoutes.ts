19c7be5f4f9edd70f71ebc6f5e5e2f2392ea13a3
sanskar <poudelsanskar@gmail.com>
2024-03-28 13:42:52 +0545

    signup completed

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
 import { isAuthenticated } from "../middlewares/auth";
 import { permissionCheck } from "../middlewares/permissionCheck";
-import { Login } from "../controllers/auth";
+import { Login, Signup } from "../controllers/auth";
 
 const router = express.Router();
+
+router.post("/signup", Signup);
 
 router.post("/login", Login);
 
