e2225adb8978e236cf15a25db6438199ceb358cf
sanskar <poudelsanskar@gmail.com>
2024-03-28 12:16:21 +0545

    initial making admin

--- Backend/database.sql
+++ Backend/database.sql
@@ -16,15 +16,23 @@ CREATE TABLE users (
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   role_id INT,
-  FOREIGN KEY (role_id) REFERENCES roles(id) 
+  FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
 CREATE TABLE role_features (
   role_id INT,
   feature_id INT,
-  enabled BOOLEAN NOT NULL, 
-  access ENUM('none', 'read', 'write') NOT NULL, 
+  enabled BOOLEAN NOT NULL,
+  access ENUM('none', 'read', 'write') NOT NULL,
   PRIMARY KEY (role_id, feature_id),
-  FOREIGN KEY (role_id) REFERENCES roles(id), 
-  FOREIGN KEY (feature_id) REFERENCES features(id) 
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (feature_id) REFERENCES features(id)
 );
+
+INSERT INTO features (feature_name, active) VALUES ('admin', TRUE);
+
+INSERT INTO roles (role_name) VALUES ('Administrator');
+
+INSERT INTO role_features (role_id, feature_id, enabled, access) VALUES (1, 1, TRUE, 'write');
+
+INSERT INTO users (first_name, last_name, email, password, role_id) VALUES ('Admin', 'User', 'admin@example.com', 'admin_password', 1);
