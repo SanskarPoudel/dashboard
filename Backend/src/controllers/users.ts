933865d0e3a5af76b69aa06d94d037520966ddcc
sanskar <poudelsanskar@gmail.com>
2024-03-29 12:20:28 +0545

    ASSIGN ROLE API

--- Backend/src/controllers/users.ts
+++ Backend/src/controllers/users.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Feature, Role, RoleFeature, User } from "../models";
+import { CustomRequest } from "../middlewares/auth";
 
 export const AllUsers = async (req: Request, res: Response) => {
   try {
@@ -28,6 +29,89 @@ export const AllUsers = async (req: Request, res: Response) => {
     res.status(500).json({
       success: false,
       message: "An error occurred while fetching users",
+    });
+  }
+};
+
+export const assignRole = async (req: CustomRequest, res: Response) => {
+  try {
+    const { role_id, user_id } = req.body as {
+      role_id: number;
+      user_id: number;
+    };
+
+    if (req.user?.id === user_id) {
+      return res.status(200).json({
+        success: false,
+        message: "You cannot update your own role",
+      });
+    }
+
+    const userExists = await User.findOne({
+      where: {
+        id: user_id,
+      },
+    });
+
+    if (!userExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const roleExists = await Role.findOne({
+      where: {
+        id: role_id,
+      },
+    });
+
+    if (!roleExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Role not found",
+      });
+    }
+
+    await User.update(
+      { role_id },
+      {
+        where: {
+          id: user_id,
+        },
+      }
+    );
+
+    const updatedUser = await User.findOne({
+      where: { id: user_id },
+      attributes: { exclude: ["password"] },
+      include: [
+        {
+          model: Role,
+          attributes: ["id", "role_name"],
+          include: [
+            {
+              model: Feature,
+              attributes: ["id", "feature_name", "active"],
+              through: {
+                attributes: ["enabled", "access"],
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Role Updated Successfully",
+      updatedUser: updatedUser,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong. Please try again",
     });
   }
 };
