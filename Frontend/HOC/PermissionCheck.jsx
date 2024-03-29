4b6f7b0f986ba150d106323c8dd8453c501a5f2b
sanskar <poudelsanskar@gmail.com>
2024-03-29 17:32:02 +0545

    permsission check HOC

---
+++ Frontend/HOC/PermissionCheck.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { useUserInfo } from "../contexts/userInfo";
+
+const withPermissions = (WrappedComponent, requiredPermissions) => {
+  return function WithPermissions(props) {
+    const { featuresAccess } = useUserInfo();
+
+    const router = useRouter();
+
+    const hasRequiredAccess = (userAccess, requiredAccess) => {
+      if (userAccess === requiredAccess) return true;
+      if (requiredAccess === "read" && userAccess === "write") return true;
+      return false;
+    };
+
+    const isAdmin = featuresAccess?.some(
+      (feature) => feature.feature === "admin"
+    );
+
+    const hasPermission =
+      isAdmin ||
+      requiredPermissions.every((requiredPermission) =>
+        featuresAccess?.some(
+          (userFeature) =>
+            userFeature.feature === requiredPermission.feature &&
+            hasRequiredAccess(userFeature.access, requiredPermission.access)
+        )
+      );
+
+    React.useEffect(() => {
+      if (!hasPermission) {
+        router.push("/");
+      }
+    }, [hasPermission, router]);
+
+    if (hasPermission) {
+      return <WrappedComponent {...props} />;
+    }
+
+    return null;
+  };
+};
+
+export default withPermissions;
