b258672354a75bd6f1333284d09e3ccce26084e6
sanskar <poudelsanskar@gmail.com>
2024-03-29 13:40:29 +0545

    update and delete feature

--- Backend/src/controllers/features.ts
+++ Backend/src/controllers/features.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Feature } from "../models";
+import { Feature, RoleFeature } from "../models";
 
 export const allFeatures = async (req: Request, res: Response) => {
   try {
@@ -58,5 +58,71 @@ export const createFeature = async (req: Request, res: Response) => {
       success: false,
       message: "Something went wrong. Try Again Later.",
     });
+  }
+};
+
+export const updateFeature = async (req: Request, res: Response) => {
+  try {
+    const { id, status } = req.body as { id: number; status: boolean };
+
+    await Feature.update(
+      { status: status },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Feature Updated Successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong. Try Again Later",
+    });
+  }
+};
+
+export const deleteFeature = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.query;
+
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Id is required",
+      });
+    }
+
+    await Feature.destroy({
+      where: {
+        id: id,
+      },
+    });
+
+    const roleFeaturesExists = await RoleFeature.findAll({
+      where: {
+        feature_id: id,
+      },
+    });
+
+    if (roleFeaturesExists.length > 0) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "This feature is assigned to a role . Please remove it to continue",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Feature deleted successfully",
+    });
+  } catch (err) {
+    console.log(err);
   }
 };
