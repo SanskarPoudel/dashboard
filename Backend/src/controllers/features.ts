3403a0787de0eeb2a45ae14d46d70595b3639a56
sanskar <poudelsanskar@gmail.com>
2024-03-29 12:19:51 +0545

    CREATE FEATURE API

--- Backend/src/controllers/features.ts
+++ Backend/src/controllers/features.ts
@@ -17,3 +17,46 @@ export const allFeatures = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const createFeature = async (req: Request, res: Response) => {
+  try {
+    const { name, active } = req.body as { name: string; active: boolean };
+
+    if (!name || !active) {
+      return res.status(404).json({
+        success: false,
+        message: "Please enter required fields",
+      });
+    }
+
+    const featureExists = await Feature.findOne({
+      where: {
+        feature_name: name,
+      },
+    });
+
+    if (featureExists) {
+      return res.status(200).json({
+        success: false,
+        message: "Feature with this name already exists",
+      });
+    }
+
+    const feature = await Feature.create({
+      feature_name: name,
+      active: active,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Feature Created Successfully",
+      feature: feature,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong. Try Again Later.",
+    });
+  }
+};
