b6f316fd2e99e2fb2d6395ddd0ab0aa54567000f
sanskar <poudelsanskar@gmail.com>
2024-03-29 16:09:14 +0545

    user routes deatils included

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -2,13 +2,20 @@ import express, { Router } from "express";
 import { isAuthenticated } from "../middlewares/auth";
 import { permissionCheck } from "../middlewares/permissionCheck";
 import { Login, Signup } from "../controllers/auth";
-import { AllUsers, assignRole, removeRole } from "../controllers/users";
+import {
+  AllUsers,
+  assignRole,
+  removeRole,
+  userDetails,
+} from "../controllers/users";
 
 const router = express.Router();
 
 router.post("/signup", Signup);
 
 router.post("/login", Login);
+
+router.get("/details", isAuthenticated, userDetails);
 
 router.get(
   "/all",
