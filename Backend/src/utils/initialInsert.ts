0161ad0cf15f7953404cb535b66efe0ebc1e2c31
SanskarPoudel <72004392+SanskarPoudel@users.noreply.github.com>
2024-03-30 17:12:05 +0545

    insertInitial data

--- Backend/src/utils/initialInsert.ts
+++ Backend/src/utils/initialInsert.ts
@@ -6,8 +6,36 @@ export const insertInitialData = async () => {
       active: true,
     });
 
+    const feature2: any = await Feature.create({
+      feature_name: "reviewleads",
+      active: true,
+    });
+
+    const feature3: any = await Feature.create({
+      feature_name: "addfaqs",
+      active: true,
+    });
+
     const role: any = await Role.create({
       role_name: "Administrator",
+    });
+
+    const role2: any = await Role.create({
+      role_name: "Moderator level 1",
+    });
+
+    await RoleFeature.create({
+      role_id: role2.id,
+      feature_id: feature2.id,
+      enabled: true,
+      access: "write",
+    });
+
+    await RoleFeature.create({
+      role_id: role2.id,
+      feature_id: feature3.id,
+      enabled: true,
+      access: "write",
     });
 
     await RoleFeature.create({
@@ -23,6 +51,14 @@ export const insertInitialData = async () => {
       email: "admin@example.com",
       password: "$2b$10$Qt7t0VZC74FCWi0..sAMVO5DUAXNpzSD2bpWY8HsM1tXprqTde.sW", // "admin_password"
       role_id: role.id,
+    });
+
+    await User.create({
+      first_name: "testUser",
+      last_name: "lastNametest",
+      email: "test@gmail.com",
+      password: "$2b$10$/NrpmYiqsTRTIajTzoF5UumbE/MdpZBlE61z2s1geSGs4okZk1jGa", // "test123"
+      role_id: role2.id,
     });
 
     console.log("Data inserted successfully.");
