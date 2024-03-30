d86e23787ce72719b5a359da4056b0143cd07b50
SanskarPoudel <72004392+SanskarPoudel@users.noreply.github.com>
2024-03-30 17:44:55 +0545

    database update full with data

--- Backend/database.sql
+++ Backend/database.sql
@@ -1,38 +1,145 @@
-CREATE TABLE features (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  feature_name VARCHAR(255) NOT NULL,
-  active BOOLEAN NOT NULL
-);
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: testdb
+-- ------------------------------------------------------
+-- Server version	8.0.36
 
-CREATE TABLE roles (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  role_name VARCHAR(255) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  role_id INT,
-  FOREIGN KEY (role_id) REFERENCES roles(id)
-);
+--
+-- Table structure for table `features`
+--
 
-CREATE TABLE role_features (
-  role_id INT,
-  feature_id INT,
-  enabled BOOLEAN NOT NULL,
-  access ENUM('none', 'read', 'write') NOT NULL,
-  PRIMARY KEY (role_id, feature_id),
-  FOREIGN KEY (role_id) REFERENCES roles(id),
-  FOREIGN KEY (feature_id) REFERENCES features(id)
-);
+DROP TABLE IF EXISTS `features`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `features` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `feature_name` varchar(255) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO features (feature_name, active) VALUES ('admin', TRUE);
+--
+-- Dumping data for table `features`
+--
 
-INSERT INTO roles (role_name) VALUES ('Administrator');
+LOCK TABLES `features` WRITE;
+/*!40000 ALTER TABLE `features` DISABLE KEYS */;
+INSERT INTO `features` VALUES (1,'admin',1,'2024-03-30 11:26:07','2024-03-30 11:26:07'),(2,'reviewleads',1,'2024-03-30 11:26:07','2024-03-30 11:26:07'),(3,'addfaqs',1,'2024-03-30 11:26:07','2024-03-30 11:26:07'),(4,'admin',1,'2024-03-30 11:30:43','2024-03-30 11:30:43'),(5,'reviewleads',1,'2024-03-30 11:30:43','2024-03-30 11:30:43'),(6,'addfaqs',1,'2024-03-30 11:30:43','2024-03-30 11:30:43');
+/*!40000 ALTER TABLE `features` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO role_features (role_id, feature_id, enabled, access) VALUES (1, 1, TRUE, 'write');
+--
+-- Table structure for table `role_features`
+--
 
-INSERT INTO users (first_name, last_name, email, password, role_id) VALUES ('Admin', 'User', 'admin@example.com', '$2b$10$Qt7t0VZC74FCWi0..sAMVO5DUAXNpzSD2bpWY8HsM1tXprqTde.sW', 1);  -- password  is "admin_password"
+DROP TABLE IF EXISTS `role_features`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role_features` (
+  `role_id` int NOT NULL,
+  `feature_id` int NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `access` enum('none','read','write') NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`role_id`,`feature_id`),
+  UNIQUE KEY `role_features_feature_id_role_id_unique` (`role_id`,`feature_id`),
+  KEY `feature_id` (`feature_id`),
+  CONSTRAINT `role_features_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `role_features_ibfk_2` FOREIGN KEY (`feature_id`) REFERENCES `features` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_features`
+--
+
+LOCK TABLES `role_features` WRITE;
+/*!40000 ALTER TABLE `role_features` DISABLE KEYS */;
+INSERT INTO `role_features` VALUES (1,1,1,'write','2024-03-30 11:26:07','2024-03-30 11:26:07'),(2,2,1,'write','2024-03-30 11:26:07','2024-03-30 11:26:07'),(2,3,1,'write','2024-03-30 11:26:07','2024-03-30 11:26:07'),(3,4,1,'write','2024-03-30 11:30:43','2024-03-30 11:30:43'),(4,5,1,'write','2024-03-30 11:30:43','2024-03-30 11:30:43'),(4,6,1,'write','2024-03-30 11:30:43','2024-03-30 11:30:43');
+/*!40000 ALTER TABLE `role_features` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Administrator','2024-03-30 11:26:07','2024-03-30 11:26:07'),(2,'Moderator level 1','2024-03-30 11:26:07','2024-03-30 11:26:07'),(3,'Administrator','2024-03-30 11:30:43','2024-03-30 11:30:43'),(4,'Moderator level 1','2024-03-30 11:30:43','2024-03-30 11:30:43');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role_id` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Admin','User','admin@example.com','$2b$10$Qt7t0VZC74FCWi0..sAMVO5DUAXNpzSD2bpWY8HsM1tXprqTde.sW',1,'2024-03-30 11:26:07','2024-03-30 11:26:07'),(2,'testUser','lastNametest','test@gmail.com','$2b$10$/NrpmYiqsTRTIajTzoF5UumbE/MdpZBlE61z2s1geSGs4okZk1jGa',2,'2024-03-30 11:26:07','2024-03-30 11:26:07');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-30 17:43:42
