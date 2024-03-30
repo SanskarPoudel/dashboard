4202fcba3b02e9d4c72851babec0ef60471ac599
SanskarPoudel <72004392+SanskarPoudel@users.noreply.github.com>
2024-03-30 16:36:31 +0545

    sidebar more section added for demo

--- Frontend/src/components/layout/SideBar.jsx
+++ Frontend/src/components/layout/SideBar.jsx
@@ -9,32 +9,54 @@ const SideBar = () => {
   const finalFeatures = featuresAccess?.map((feat) => feat.feature);
 
   const router = useRouter();
-  const navigation = [
-    { name: "Home", href: "/dashboard" },
-    finalFeatures?.includes("admin") && {
+
+  const allNavigationItems = [
+    { name: "Home", href: "/dashboard", condition: true },
+    {
       name: "Features",
       subName: "( admin only )",
       href: "/features",
+      condition: finalFeatures?.includes("admin"),
     },
-    finalFeatures?.includes("admin") && {
+    {
       name: "Roles",
       subName: "( admin only )",
       href: "/roles",
+      condition: finalFeatures?.includes("admin"),
     },
-    finalFeatures?.includes("admin") && {
+    {
       name: "Users",
       subName: "( admin only )",
       href: "/users",
+      condition: finalFeatures?.includes("admin"),
     },
-  ].filter(Boolean);
+    {
+      name: "Review Leads",
+      href: "/reviewleads",
+      condition:
+        finalFeatures?.includes("reviewleads") ||
+        finalFeatures?.includes("admin"),
+    },
+    {
+      name: "Add Faqs",
+      href: "/addfaqs",
+      condition:
+        finalFeatures?.includes("addfaqs") || finalFeatures?.includes("admin"),
+    },
+  ];
+
+  const navigation = allNavigationItems
+    .filter((item) => item.condition)
+    .map(({ condition, ...item }) => item);
+
   return (
     <aside
-      className={`fixed top-0 left-0 w-64 bg-gray-800 text-white h-full transform transition-transform duration-300 ease-in-out`}
+      className="fixed top-0 left-0 w-64 bg-gray-800 text-white h-full transform transition-transform duration-300 ease-in-out"
       style={{ top: "4rem" }}
     >
       <nav className="p-5 space-y-4">
-        {navigation.map((item) => (
-          <Link href={item.href} key={item.name} passHref>
+        {navigation.map((item, index) => (
+          <Link href={item.href} key={index} passHref>
             <div
               className={`cursor-pointer block py-2 px-4 hover:bg-gray-600 rounded-lg ${
                 router.pathname === item.href ? "bg-gray-700" : ""
