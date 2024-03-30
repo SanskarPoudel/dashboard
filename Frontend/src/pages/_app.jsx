41ca78b55bd9abdb916d33cf6d7751e980af7554
SanskarPoudel <72004392+SanskarPoudel@users.noreply.github.com>
2024-03-30 15:45:40 +0545

    app

--- Frontend/src/pages/_app.jsx
+++ Frontend/src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { UserInfoProvider } from "../../contexts/userInfo";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
